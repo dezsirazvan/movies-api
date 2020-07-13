41dd620619f3c75c9c7e48aef4e220cbc9fccf53
Dezsi Razvan <dezsirazvan@gmail.com>
2020-07-13 19:24:15 +0300

    Fix serializer

--- app/serializers/api/v1/purchase_serializer.rb
+++ app/serializers/api/v1/purchase_serializer.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class Api::V1::PurchaseSerializer < ActiveModel::Serializer
-  attributes  :id, :remaining_time
+  attributes  :id, :remaining_time_in_hours
  
   belongs_to :movie, optional: true
   belongs_to :season, optional: true
 
-  def remaining_time
-    time_diff = Time.now - object.created_at
+  def remaining_time_in_hours
+    time_diff = (object.created_at + 2.days) - DateTime.current 
 
-    Time.at(2.days - time_diff.to_i.abs).utc.strftime "%H:%M:%S"
+    (time_diff / 1.hour)
   end
 end
     
